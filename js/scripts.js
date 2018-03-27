28d2c272da68df46c1d9d56b8f216e459f345e56
Brenda Franco and John Linneman <john@>
2018-03-27 09:30:26 -0700

    Add logic to clear all list items from ul on submit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
+    event.preventDefault();
+
+    $("ul").empty();
 
     var inputNumber1 = parseInt($("#number1").val());
     var inputNumber2 = parseInt($("#number2").val());
 
     for (var index = 0; index <= inputNumber1; index += inputNumber2) {
-      // debugger;
+
       $("#result").append("<li>" + index + "</li>");
     }
-    event.preventDefault();
+
   });
 });

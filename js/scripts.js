5eec8d73ac522764fc4e09a4e64ab82db50f283e
Brenda Franco and John Linneman <john@>
2018-03-27 09:39:02 -0700

    add if statement for incorrect input

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,14 @@ $(document).ready(function() {
     var inputNumber1 = parseInt($("#number1").val());
     var inputNumber2 = parseInt($("#number2").val());
 
-    for (var index = 0; index <= inputNumber1; index += inputNumber2) {
-
-      $("#result").append("<li>" + index + "</li>");
+    if (inputNumber1 <= 0 || inputNumber2 <= 0) {
+      alert("input positive value");
+    } else if (inputNumber1 < inputNumber2) {
+      alert("Enter a max number larger than the increment number");
+    } else {
+      for (var index = 0; index <= inputNumber1; index += inputNumber2) {
+        $("#result").append("<li>" + index + "</li>");
+      }
     }
 
   });
